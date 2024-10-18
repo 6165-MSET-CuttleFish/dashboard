d524a2f6cc7c754731b2af27c34090a719dee7a6
aaditn <35712393+aaditn@users.noreply.github.com>
2024-10-18 12:16:38 -0700

    Alpha Release

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleConstants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.cuttlefish.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/*
+ * Some additional constants for testing.
+ */
+@Config
+public class SampleConstants {
+    public static DcMotorSimple.Direction DIR = DcMotorSimple.Direction.FORWARD;
+    public static boolean FF_ENABLED = false;
+}
