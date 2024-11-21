794f62eb7130f258b2053d8a8ddcd51c7b34ab0c
Alan Rong <alanrong1@gmail.com>
2024-11-20 21:13:10 -0800

    test

--- DashboardCore/src/main/java/com/cuttlefish/dashboard/CustomVariableConsumer.java
+++
@@ -1,7 +0,0 @@
-package com.cuttlefish.dashboard;
-
-import com.cuttlefish.dashboard.config.variable.CustomVariable;
-
-public interface CustomVariableConsumer {
-    void accept(CustomVariable customVariable);
-}
--- DashboardCore/src/main/java/com/cuttlefish/dashboard/SendFun.java
+++
@@ -1,7 +0,0 @@
-package com.cuttlefish.dashboard;
-
-import com.cuttlefish.dashboard.message.Message;
-
-public interface SendFun {
-    void send(Message message);
-}
